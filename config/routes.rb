57abce8bf881ca3a4a8c0f78a69545f88d1a2f6a
michellinh <linhjava98@gmail.com>
2019-10-31 10:56:56 +0700

    chapter 12: Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
     resources :users
     resources :account_activations, only: :edit
+    resources :password_resets, only: %i(new create edit update)
   end
 end
+
